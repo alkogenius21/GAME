8125f51a346b4771238c09d1f2b3ad3e13a0c47e
alkogenius21 <obuhoff2016@yandex.ru>
2022-01-27 00:58:51 +0300

    ,

--- GAME/player.py
+++ GAME/player.py
@@ -3,14 +3,15 @@ import random
 from size import *
 import math
 
-
 WIDTH = disp_width
 HEIGHT = disp_height
 
 GREEN = (0, 255, 0)
 
 class Player(pygame.sprite.Sprite):
+
     def __init__(self):
+
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('assets/png2.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (120, 120))
@@ -20,6 +21,7 @@ class Player(pygame.sprite.Sprite):
         self.speedx = 0
 
     def keyboard(self):
+
         self.speedx = 0
         self.speedy = 0
         keystate = pygame.key.get_pressed()
@@ -37,10 +39,13 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+
     def rotate(self):
+
         self.position = pygame.mouse.get_pos()
         self.angle = math.atan2(self.position[1] - (self.rect.bottom + 40), self.position[0] + (self.rect.centerx + 50))
         self.rect = pygame.transform.rotate(self.image, 360 - self.angle * 57.9)
 
     def update(self):
+
         self.keyboard()
